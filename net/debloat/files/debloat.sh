ae9efa5dc2fdb0402eb3c76ba0797a8f42890656
Dave Taht <d+git@taht.net>
2011-05-09 00:02:47 -0600

    Basic outline of debloat package

--- net/debloat/files/debloat.sh
+++ net/debloat/files/debloat.sh
@@ -1,13 +1,3 @@
-# debloat.sh - IPv6-in-IPv4 tunnel backend
+# debloat.sh - utilities for debloating interfaces
 # Copyright (c) 2010 OpenWrt.org
-
-find_6to4_wanif() {
-	local if=$(ip -4 r l e 0.0.0.0/0); if="${if#default* dev }"; if="${if%% *}"
-	[ -n "$if" ] && grep -qs "^ *$if:" /proc/net/dev && echo "$if"
-}
-
-find_6to4_wanip() {
-	local ip=$(ip -4 a s dev "$1"); ip="${ip#*inet }"
-	echo "${ip%%[^0-9.]*}"
-}
 
