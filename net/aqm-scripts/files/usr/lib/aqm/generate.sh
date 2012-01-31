b8d2f8de825b40e83461f8bad0ffe22efbd49e26
Dave Taht <d@taht.net>
2012-01-31 18:04:16 +0100

    Continued attempt at aqm co-existence

--- net/aqm-scripts/files/usr/lib/aqm/generate.sh
+++ net/aqm-scripts/files/usr/lib/aqm/generate.sh
@@ -264,7 +264,7 @@ cls_var() {
 }
 
 tcrules() {
-	dir=/usr/lib/qos
+	dir=/usr/lib/aqm
 	[ -e $dir/tcrules.awk ] || dir=.
 	echo "$cstr" | awk \
 		-v device="$dev" \
@@ -464,10 +464,10 @@ stop_firewall() {
 
 C="0"
 INTERFACES=""
-[ -e ./qos.conf ] && {
-	. ./qos.conf
+[ -e ./aqm.conf ] && {
+	. ./aqm.conf
 	config_cb
-} || config_load qos
+} || config_load aqm
 
 C="0"
 for iface in $INTERFACES; do
