a99108f5d120a6d2fe06ba9d50372b60d38fee2f
Dave Taht <dave.taht@bufferbloat.net>
2012-04-04 16:37:36 -0700

    Makefile

--- ipv6/ipv6policy/files/ipv6policy.sh
+++ ipv6/ipv6policy/files/ipv6policy.sh
@@ -2,8 +2,32 @@
 # Copyright (c) 2012 OpenWrt.org
 
 # This code is nowhere near done yet
+# Get default device (ge00)
 
-notes() {
-#config_get ipv6policy
-#generate-rfc4193-addr se00
+find_ethernet_device() {
+	for i in /sys/class/net/*
+	do
+	done
 }
+
+rfc34193() {
+	local dev=`find_ethernet_device`
+	generate_rfc3193 $dev
+}
+
+get_ula() {
+	config_get ipv6policy ula
+	if enable
+	[ "$net" == "" ] && net=`rfc4193` 
+	if disable
+	config_get network
+}
+
+get_6to4() {
+	config_get ipv6policy ula
+}
+
+get_pd() {
+}
+
+
--- net/aqm-scripts/files/usr/lib/aqm/generate.sh
+++ net/aqm-scripts/files/usr/lib/aqm/generate.sh
@@ -426,6 +426,7 @@ start_cg() {
 		append up "ip6tables -t mangle -A OUTPUT -o $device -j qos_${cg}" "$N"
 		append up "ip6tables -t mangle -A FORWARD -o $device -j qos_${cg}" "$N"
 	done
+# The only thing I think that's wrong with this is connmark...
 	cat <<EOF
 $INSMOD
 iptables -t mangle -N qos_${cg} >&- 2>&-
@@ -439,7 +440,7 @@ iptables -t mangle -A qos_${cg} -m mark --mark 0/0xff -j qos_${cg}_ct
 ip6tables -t mangle -A qos_${cg} -j CONNMARK --restore-mark --mask 0xff
 ip6tables -t mangle -A qos_${cg} -m mark --mark 0/0xff -j qos_${cg}_ct
 $pktrules
-ip6tables -t mangle -A qos_${cg} --source fe80::/8 -j MARK --set-mark 1/0xff
+ip6tables -t mangle -A qos_${cg} --source fe80::/10 -j MARK --set-mark 1/0xff
 $up$N${down:+${down}$N}
 EOF
 	unset INSMOD
