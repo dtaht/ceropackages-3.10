5e8d5ef63eaf91d4c6a741c6cdc76a346093f9c2
Dave Taht <dave.taht@bufferbloat.net>
2014-01-18 17:25:10 -0500

    redir: detect ipv6 addressing better

--- utils/cerowrt-chrome/files/etc/bgi-bin/redir.sh
+++ utils/cerowrt-chrome/files/etc/bgi-bin/redir.sh
@@ -2,5 +2,10 @@
 echo "Content-Type: text/html; charset=UTF-8
 Pragma: no-cache
 "
-
+S=`echo ${SERVER_NAME}` | sed 's/:/ /g'`
+if [ "${S}" != ${SERVER_NAME} ]
+then
+echo "<html><head><meta http-equiv=\"REFRESH\" content=\"0;url=https://[${SERVER_NAME}]:81\"></head><body>Redirecting</body></html>"
+else
 echo "<html><head><meta http-equiv=\"REFRESH\" content=\"0;url=https://${SERVER_NAME}:81\"></head><body>Redirecting</body></html>"
+fi
