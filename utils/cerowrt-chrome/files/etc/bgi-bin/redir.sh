812c4d060be5d4ced84e9213c15a347af6f3a845
Dave Taht <dave.taht@bufferbloat.net>
2014-01-18 15:48:55 -0500

    change redir.sh to https

--- utils/cerowrt-chrome/files/etc/bgi-bin/redir.sh
+++ utils/cerowrt-chrome/files/etc/bgi-bin/redir.sh
@@ -3,4 +3,4 @@ echo "Content-Type: text/html; charset=UTF-8
 Pragma: no-cache
 "
 
-echo "<html><head><meta http-equiv=\"REFRESH\" content=\"0;url=http://${SERVER_NAME}:81\"></head><body>Redirecting</body></html>"
+echo "<html><head><meta http-equiv=\"REFRESH\" content=\"0;url=https://${SERVER_NAME}:81\"></head><body>Redirecting</body></html>"
