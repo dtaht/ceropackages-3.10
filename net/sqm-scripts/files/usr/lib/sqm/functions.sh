674d10ef268477608ce04c08c0fbcba69be7201a
Dave Taht <dave.taht@bufferbloat.net>
2014-04-09 14:46:10 -0700

    tc - left in debugging code

--- net/sqm-scripts/files/usr/lib/sqm/functions.sh
+++ net/sqm-scripts/files/usr/lib/sqm/functions.sh
@@ -46,7 +46,7 @@ do_modules() {
 [ -z "$SQUASH_DSCP" ] && SQUASH_DSCP="1"
 [ -z "$IQDISC_OPTS" ] && IQDISC_OPTS=""
 [ -z "$EQDISC_OPTS" ] && EQDISC_OPTS=""
-[ -z "$TC" ] && TC=`echo tc`
+[ -z "$TC" ] && TC=`which tc`
 #[ -z "$TC" ] && TC="logger tc"# this redirects all tc calls into the log
 [ -z "$INSMOD" ] && INSMOD=`which insmod`
 [ -z "TARGET" ] && TARGET="5ms"
