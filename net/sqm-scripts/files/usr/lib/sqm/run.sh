effaffa1f2a8f60c305391f6e7025b95e72e9d7a
Sebastian Moeller <moeller0@gmx.de>
2014-10-09 21:51:20 +0200

    Fix SQM tear down on interfaces that are just deleted or changed in uci
    Up until now SQM tried to use the uci config file /etc/config/sqm
    to figure out on which interfaces it was running. Unfortunatelly,
    the uci config file is a bad place to save this state, as the user
    can change the interface for a given section and thereby wiping out
    the information about the previous interface, whileSQM might still
    be active on the old interface. To partly deal with this issue
    SQM had started to create its own state file to mark interfaces
    with active SQM instances. Instead of using the interfaces from the
    config file just use these state fils to figure out which interfaces
    might need to be stopped. And since all current qos scrpts start out
    with tearing down any traffic shaping infrastructure on its main
    interface and the associated IFB, we just stop all active SQM instances
    before potentially starting new ones. This needs testing...

--- net/sqm-scripts/files/usr/lib/sqm/run.sh
+++ net/sqm-scripts/files/usr/lib/sqm/run.sh
@@ -3,26 +3,42 @@
 . /lib/functions.sh
 
 STOP=$1
+ACTIVE_STATE_PREFIX="SQM_active_on_"
 ACTIVE_STATE_FILE_DIR="/var/run/SQM"
 mkdir -p ${ACTIVE_STATE_FILE_DIR}
 
-config_load sqm
+# the current uci config file does not necessarily contain sections for all interfaces with active
+# SQM instances, so use the ACTIVE_STATE_FILES to detect the interfaces on which to stop SQM.
+# Currently the .qos scripts start with stopping any existing traffic shaping so this should not
+# effectively change anything...
+PROTO_STATE_FILE_LIST=$( ls ${ACTIVE_STATE_FILE_DIR}/${ACTIVE_STATE_PREFIX}* 2> /dev/null )
+for STATE_FILE in ${PROTO_STATE_FILE_LIST} ; do
+    if [ -f ${STATE_FILE} ] ;
+    then
+	STATE_FILE_BASE_NAME=$( basename ${STATE_FILE} )
+	CURRENT_INTERFACE=${STATE_FILE_BASE_NAME:${#ACTIVE_STATE_PREFIX}:$(( ${#STATE_FILE_BASE_NAME} - ${#ACTIVE_STATE_PREFIX} ))}        
+	logger -t SQM -s "Stopping SQM on interface: ${CURRENT_INTERFACE}"
+	/usr/lib/sqm/stop.sh ${CURRENT_INTERFACE}
+	rm ${STATE_FILE}	# well, we stop it so it is not running anymore and hence no active state file needed...
+    fi
+done
 
-#IFB_NUM=0
+config_load sqm
 
 run_simple_qos() {
 	local section="$1"
 	export IFACE=$(config_get "$section" interface)
-	ACTIVE_STATE_FILE_FQN="${ACTIVE_STATE_FILE_DIR}/SQM_active_on_${IFACE}"	# this marks interfaces as active with SQM
-	[ -f "${ACTIVE_STATE_FILE_FQN}" ] && logger -t SQM -s "${ACTIVE_STATE_FILE_FQN}"
-
+	ACTIVE_STATE_FILE_FQN="${ACTIVE_STATE_FILE_DIR}/${ACTIVE_STATE_PREFIX}${IFACE}"	# this marks interfaces as active with SQM
+	[ -f "${ACTIVE_STATE_FILE_FQN}" ] && logger -t SQM -s "Uh, oh, ${ACTIVE_STATE_FILE_FQN} should already be stopped."	# Not supposed to happen
 
 	if [ $(config_get "$section" enabled) -ne 1 ];
 	then
 	    if [ -f "${ACTIVE_STATE_FILE_FQN}" ];
 	    then
+		# this should not be possible, delete after testing
 		local SECTION_STOP="stop"	# it seems the user just de-selected enable, so stop the active SQM
 	    else
+		logger -t SQM -s "SQM for interface ${IFACE} is not enabled, skipping over..."
 		return 0	# since SQM is not active on the current interface nothing to do here
 	    fi
 	fi
@@ -47,18 +63,16 @@ run_simple_qos() {
 	export SQUASH_DSCP=$(config_get "$section" squash_dscp)
 	export SQUASH_INGRESS=$(config_get "$section" squash_ingress)
 
-#	export DEV="ifb${IFB_NUM}"
-#	IFB_NUM=$(expr $IFB_NUM + 1)
-
 	export QDISC=$(config_get "$section" qdisc)
 	export SCRIPT=/usr/lib/sqm/$(config_get "$section" script)
 
+#	# there should be nothing left to stop, so just avoid calling the script
 	if [ "$STOP" == "stop" -o "$SECTION_STOP" == "stop" ];
 	then 
-	     /usr/lib/sqm/stop.sh
-	     [ -f ${ACTIVE_STATE_FILE_FQN} ] && rm ${ACTIVE_STATE_FILE_FQN}	# conditional to avoid errors ACTIVE_STATE_FILE_FQN does not exist anymore
-	     $(config_set "$section" enabled 0)	# this does not save to the config file only to the loaded memory representation
-	     logger -t SQM -s "SQM qdiscs on ${IFACE} removed"
+#	     /usr/lib/sqm/stop.sh
+#	     [ -f ${ACTIVE_STATE_FILE_FQN} ] && rm ${ACTIVE_STATE_FILE_FQN}	# conditional to avoid errors ACTIVE_STATE_FILE_FQN does not exist anymore
+#	     $(config_set "$section" enabled 0)	# this does not save to the config file only to the loaded memory representation
+#	     logger -t SQM -s "SQM qdiscs on ${IFACE} removed"
 	     return 0
 	fi
 	logger -t SQM -s "Queue Setup Script: ${SCRIPT}"
